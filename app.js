5ad1d0e157b8c6a2a6a9e586b6629759edebe9dd
Will <willpearson9120@gmail.com>
2024-07-27 19:51:47 +0100

    First commit Built main arithmetic functions

---
+++ app.js
@@ -0,0 +1,33 @@
+"use strict";
+
+function add(...nums) {
+  let sum = 0;
+  for (let num of nums) {
+    sum += num;
+  }
+  return sum;
+}
+
+function subtract(...nums) {
+  let firstNum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    firstNum -= nums[i];
+  }
+  return firstNum;
+}
+
+function multiply(...nums) {
+  let firstNum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    firstNum *= nums[i];
+  }
+  return firstNum;
+}
+
+function divide(...nums) {
+  let firstNum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    firstNum /= nums[i];
+  }
+  return firstNum;
+}
